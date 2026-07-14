1591656cad2e257ac5dba679a585bee39b11b5fd
Nada Hossam <nadodhossam29@gmail.com>
2026-07-14 19:28:36 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Scaffold(appBar:AppBar(title:Text('سبحه'),backgroundColor: Colors.deepPurpleAccent,),backgroundColor: const Color.fromARGB(255, 251, 172, 198), body: Center(child: Text('صلي علي النبي'),),) ,));
+}
+
+
